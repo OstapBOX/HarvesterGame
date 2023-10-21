787bfc1872c35e0a49d6fadbf37f4ec574706e8a
Ostap Korobka <76116862+OstapBOX@users.noreply.github.com>
2023-10-22 01:42:42 +0300

    Revert "First Build On Version 2021.3.28f1"
    This reverts commit e659b74e9ed107e48251881adb638332f7b808de.

--- Harvester/Harvester/Assets/Scripts/Shop/IAPManager.cs
+++ Harvester/Harvester/Assets/Scripts/Shop/IAPManager.cs
@@ -364,8 +364,4 @@ public class IAPManager : Singleton<IAPManager>, IStoreListener {
 		// A product purchase attempt did not succeed. Check failureReason for more detail. Consider sharing this reason with the user.
 		Debug.Log(string.Format("OnPurchaseFailed: FAIL. Product: '{0}', PurchaseFailureReason: {1}", product.definition.storeSpecificId, failureReason));
 	}
-
-    public void OnInitializeFailed(InitializationFailureReason error, string message) {
-        throw new NotImplementedException();
-    }
 }
